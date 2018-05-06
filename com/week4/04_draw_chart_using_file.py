2759eb2c206d47f1d1c15e020b91742e00235452
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-05-06 14:04:12 +0900

    numpy

--- com/week4/04_draw_chart_using_file.py
+++ com/week4/04_draw_chart_using_file.py
@@ -1,18 +1,14 @@
-import matplotlib.pylab as plt
 import numpy as np
+import matplotlib.pylab as plt
 
-f1 = open("./1_to_100.txt", mode='r')
+f1 = open("./random_number.txt", mode='r')
 lines = f1.readlines()
 
 datas = []
 for line in lines:
     datas.append(line.replace("\n", ""))
 
-x = np.arange(1, len(datas) + 1, 1)
-y = np.array(datas)
+print(datas)
 
-
-print(len(x))
-print(len(y))
-plt.plot(x, y)
+plt.plot(np.arange(1, len(datas)+1, 1), datas)
 plt.show()
