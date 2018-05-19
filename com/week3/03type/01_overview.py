d1ed6cf3f85adc64ca78eb6096bf6eead6fa5a46
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-05-19 11:25:54 +0900

    add type

---
+++ com/week3/03type/01_overview.py
@@ -0,0 +1,31 @@
+# 1 l i I
+'''
+an_l
+an_1
+an_i
+an_I
+'''
+
+# 1  b1 a1
+# "1" 1     0001 /0x4327
+
+result = 1 + 1  # 숫자형 int flot double
+print(result)
+
+result2 = "1" + "1" # 문자형 string
+print(result2)
+
+print(type(result))
+print(type(result2))
+
+import bs4
+
+print(type(bs4))
+print(type(1))
+
+'''
+    ll000 - 2l00
+
+    "1"
+    "asoentuhaosecnuhrsc',.huaosnetuhaeosrcuh',a.sntuhaoesnuthaoesntuhaoestnuhaeostuhaosentuhasonetuhsaoetnuhasoetuh
+'''
